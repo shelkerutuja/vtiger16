39195af069b267cb2591be5f3d78d7047e20be4c
shelkerutuja <135001465+shelkerutuja@users.noreply.github.com>
2023-05-31 03:05:19 -0930

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,9 @@
+class Array2 
+{
+	public static void main(String[] args) 
+	{
+		//case 2
+		int[]a=new int[5];
+		System.out.println(a[3]);
+		}
+}
---
+++ Array3.java
@@ -0,0 +1,11 @@
+class Array3
+{
+	public static void main(String[] args) 
+	{
+		//case 3
+		int[]a=new int[5];
+		a[0]=10;
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+	}
+}
